2718297a5be7ae5520b7da7e02593566f0ed70aa
ladaco <euge@email.ua>
2023-01-30 23:13:41 +0200

    Update csvtxt.py

--- csvtxt.py
+++ csvtxt.py
@@ -3,3 +3,4 @@
 with open('btc_balance_sorted.csv', 'r') as f_in, open('btc_balance_sorted.txt', 'w') as f_out:
     content = f_in.read().replace(',^[*]', ' ')
     f_out.write(content)
+    f_out.close()
